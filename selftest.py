1c92312a2fa2e4d2519f153be148c8f0f4c7b0ce
Oleg Korobkin <you@example.com>
2016-06-08 22:45:11 -0600

    (+) test_abu_evolution: also working

--- selftest.py
+++ selftest.py
@@ -63,6 +63,22 @@ class TestAbuChart(unittest.TestCase):
             self.assertTrue(os.path.exists(png_file))
 
 
+    def test_abu_evolution(self):
+        from NuGridPy import ppn, utils
+        import matplotlib.pylab as mpy
+
+        symbs=utils.symbol_list('lines2')
+        x=ppn.xtime('.')
+        specs=['PROT','HE  4','C  12','N  14','O  16']
+        i=0
+        for spec in specs:
+            x.plot('time',spec,logy=True,logx=True,shape=utils.linestyle(i)[0],show=False,title='')
+            i += 1
+        mpy.ylim(-5,0.2)
+        mpy.legend(loc=0)
+        mpy.xlabel('$\log t / \mathrm{min}$')
+        mpy.ylabel('$\log X \mathrm{[mass fraction]}$')
+        mpy.savefig('abu_evolution.png')
 
 if __name__ == '__main__':
     unittest.main()
