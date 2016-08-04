ce2a3ab479a00331685e041da9de634178bc2af6
Benoit Cote <benoit.cote.4@ulaval.ca>
2016-08-04 17:35:52 -0400

    BC: update benoittest_py

--- regression_tests/benoittest.py
+++ regression_tests/benoittest.py
@@ -8,11 +8,11 @@ class Test_README_File(unittest.TestCase):
 
     def test_first_line(self):
         with open('README', 'r') as f:
-        first_line = f.readline()
-        if first_line == 'NuGridpy':
-            print 'Need to pass'
-        else:
-            print 'Need to fail'
+            first_line = f.readline()
+            if first_line == 'NuGridpy':
+                print 'Need to pass'
+            else:
+                print 'Need to fail'
         f.close()
 
 
