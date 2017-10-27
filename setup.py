befacd80acfd693c19a63598ef84f76c337c96a7
Falk Herwig <fherwig@uvic.ca>
2017-10-26 18:12:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "nugridpy",
-    version = "0.11",
+    version = "0.7.1",
     packages = find_packages(),
     install_requires = ["numpy", "setuptools","future"],
     author = "NuGrid collaboration",
