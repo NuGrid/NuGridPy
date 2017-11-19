9414d086874bd94ac904bddccd6fc2c566183d9e
fherwig <fherwig@uvic.ca>
2017-11-19 07:55:27 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='NuGridpy',
       version='0.7.4', 
       description='Python tools for NuGrid',
-      long_description=open('README.rst', 'r').read(), # get discription from README
+#      long_description=open('README.rst', 'r').read(), # get discription from README
       # pandoc --from=markdown --to=rst --output=README.rst README.md
       author='NuGrid Team',
       author_email='fherwig@uvic.ca',
