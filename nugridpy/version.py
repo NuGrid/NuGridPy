df64597597acc0eb0b2377ee6110c678d0218d7b
Falk Herwig <fherwig@uvic.ca>
2025-06-27 01:39:58 -0700

    upgrade to version 0.8.0

--- nugridpy/version.py
+++ nugridpy/version.py
@@ -1,7 +1,7 @@
 """NuGridPy package version"""
 
 import subprocess
-__version__ = '0.7.6'
+__version__ = '0.8.0'
 try:
     commit = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
     __version__ += f"+{commit}"
