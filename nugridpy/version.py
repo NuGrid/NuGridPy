92c066a42a979fc9c0d648a5cf62815f281d0f35
Falk Herwig <fherwig@uvic.ca>
2025-06-27 01:39:32 -0700

    add hash to version

--- nugridpy/version.py
+++ nugridpy/version.py
@@ -1,3 +1,9 @@
 """NuGridPy package version"""
 
+import subprocess
 __version__ = '0.7.6'
+try:
+    commit = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+    __version__ += f"+{commit}"
+except Exception:
+    pass
